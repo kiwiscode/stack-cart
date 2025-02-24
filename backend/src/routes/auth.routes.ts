4bc430101071525c184cfa7aa591e2f47b7c8b97
kiwiscode <ayktkav@gmail.com>
2025-02-24 22:39:22 +0100

    test(auth): add protected-route to test authMiddleware

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -4,8 +4,23 @@ import { authMiddleware } from "../middlewares/authMiddleware";
 
 const router = Router();
 
+interface ProtectedRouteReqBody {
+  userId: number;
+}
+
 router.post("/register", register);
 router.post("/login", login);
 router.post("/logout", authMiddleware, logout);
+router.post("/protected-route", authMiddleware, async (req, res) => {
+  try {
+    const { userId }: ProtectedRouteReqBody = req.body;
+
+    console.log("user id received for protected route:", userId);
+
+    res.status(200).json({ success: true, userId });
+  } catch (error) {
+    console.log("error:", error);
+  }
+});
 
 export default router;
